ab58f3bd708ed2ae29f5a72261faf1b1e3e093b1
mkjsym <mkjsym@naver.com>
2024-09-11 23:52:13 +0900

    2024.09.11

---
+++ Airport_Terminal_Simulator/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float speed = 4.0f;
+    public float rotSpeed = 12f;
+    private Animator animator;
+    private Vector3 dir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        dir.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        if(dir != Vector3.zero){
+            if (Mathf.Sign(transform.forward.x) != Mathf.Sign(dir.x) || Mathf.Sign(transform.forward.z) != Mathf.Sign(dir.z)){
+                transform.Rotate(0, 1, 0);
+            }
+            transform.forward = Vector3.Lerp(transform.forward, dir, rotSpeed*Time.deltaTime);
+        }
+        rigidbody.MovePosition(this.gameObject.transform.position + dir * speed * Time.deltaTime);
+
+    }
+
+}
